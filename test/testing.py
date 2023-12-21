03fcbcc1f26e42c9fae7b80952a19afbae993fb4
MaDycloud-MD <md.shoaib.i.makandar@gmail.com>
2023-12-21 14:10:17 +0530

    Image issue fixed

--- test/testing.py
+++ test/testing.py
@@ -8,9 +8,28 @@ dynamodb = boto3.client('dynamodb', region_name='ap-south-1')
 image_path = input("Enter path of the image to check: ")
 
 image = Image.open(image_path)
-stream = io.BytesIO()
-image.save(stream,format="JPEG")
-image_binary = stream.getvalue()
+
+if(image_path.endswith(".jpg")):
+        stream = io.BytesIO()
+        image.save(stream,format="JPEG")
+        image_binary = stream.getvalue() 
+    
+else:
+        # Convert image to RGB (if necessary)
+        if image.mode == "RGBA":
+            rgb_image = image.convert("RGB")
+        else:
+            rgb_image = image
+
+        # Save as JPEG
+        stream = io.BytesIO()
+        rgb_image.save(stream, format="JPEG")
+        image_binary = stream.getvalue()
+
+        # Save the converted image
+        with open("output_image.jpg", "wb") as output_file:
+            output_file.write(image_binary)
+            print("Image converted and saved as JPEG")
 
 
 response = rekognition.search_faces_by_image(
